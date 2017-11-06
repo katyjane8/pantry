26f4ee2691348e9dc3dd1cda19203cbada2785e1
katyjane8 <katy.jane8@gmail.com>
2017-11-06 13:07:43 -0700

    working with cheese stock

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,28 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+   def test_it_exists
+     pantry = Pantry.new
 
+     assert_instance_of Pantry, pantry
+   end
+
+   def test_pantry_has_stock
+     pantry = Pantry.new
+
+     assert_equal ({}), pantry.stock
+   end
+
+   def test_pantry_stock_can_be_checked
+     pantry = Pantry.new
+
+     assert_equal 0, pantry.stock_check("Cheese")
+   end
+
+   def test_pantry_can_be_restocked_with_ingredient
+     pantry = Pantry.new
+     pantry.restock("Cheese", 10)
+
+     assert_equal 10, pantry.stock_check("Cheese")
+   end
 end
